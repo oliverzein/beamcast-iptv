00ffae38f50f386404785261ebe6f460e0ad85a6
Oliver Zein <oliverzein@gmail.com>
2026-06-18 11:31:38 +0200

    feat(settings): wire epgPrefetchConcurrency consumer in renderer-epg

--- renderer-epg.js
+++ renderer-epg.js
@@ -48,7 +48,7 @@ async function fetchAndStoreEpg(account) {
     const stats = await prefetchEpgHistory(catchupStreams, {
       fetcher,
       onProgress,
-      concurrency: 4,
+      concurrency: AppSettings.get('epgPrefetchConcurrency', 4),
       perFetchTimeoutMs: 15000
     });
 
